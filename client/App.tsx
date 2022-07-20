95125fcd25ad0d107703585a50ab698aca760eee
zani-t <tembo4.t@gmail.com>
2022-07-20 19:29:53 -0400

    splash color

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// expo, react, external, internal
+import { NavigationContainer } from '@react-navigation/native';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+
+import AnimatedSplashScreen from './screens/animated-splash-screen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  type SharedStackParams = {
+    Splash: { id: string };
+    Auth: { id: string };
+    Home: { id: string };
+    Sort: { id: string };
+  };
+  
+  const { Navigator, Screen } = createSharedElementStackNavigator<SharedStackParams>();
+
+  // container id 0
+  // panel id 1
+
+  return (
+    
+    <NavigationContainer>
+      <Navigator initialRouteName='Splash' screenOptions={{headerShown: false}}>
+        <Screen name='Splash' component={AnimatedSplashScreen}/>
+      </Navigator>
+    </NavigationContainer>
+
+  );
+};
---
+++ client/screens/animated-splash-screen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+interface Props {};
+
+const AnimatedSplashScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>animated-splash-screen</Text>
+    </View>
+  );
+};
+
+export default AnimatedSplashScreen;
+
+const styles = StyleSheet.create({});
