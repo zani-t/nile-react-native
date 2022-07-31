58b844b5f1cf0f89a986188a65b05ef5baacb259
zani-t <tembo4.t@gmail.com>
2022-07-30 22:31:32 -0400

    django token setup

--- server/api/views.py
+++ server/api/views.py
@@ -1,29 +1,46 @@
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# from .serializers import ArticleSerializer
+# from .models import Article
 
 from newspaper import Article
 from newspaper import Source
 import ast
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Custom claims
+        token['email'] = user.username
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 # Newspaper query
 @csrf_exempt
+@api_view(['POST'])
 def query(request):
-    # Remove & add @api_view decorator from RestFramework
-    if request.method == 'POST':
-        url = ast.literal_eval(request.body.decode("utf-8")).get('url', 'NULL_URL')
+    url = ast.literal_eval(request.body.decode("utf-8")).get('url', 'NULL_URL')
 
-        if url == 'NULL_URL':
-            return JsonResponse({'ARTICLE_NOT_FOUND', url})
+    if url == 'NULL_URL':
+        return Response({'ARTICLE_NOT_FOUND', url})
 
-        new_article = Article(url)
-        new_article.download()
-        new_article.parse()
+    new_article = Article(url)
+    new_article.download()
+    new_article.parse()
 
-        article_info = {}
-        article_info['title'] = new_article.title if new_article.title else 'NULL_TITLE'
-        article_info['image'] = new_article.top_image if new_article.top_image else 'NULL_IMG'
-        article_info['source'] = Source(new_article.source_url).brand.upper() if Source(new_article.source_url).brand else 'NULL_BRAND'
-        article_info['url'] = url
+    article_info = {}
+    article_info['title'] = new_article.title if new_article.title else 'NULL_TITLE'
+    article_info['image'] = new_article.top_image if new_article.top_image else 'NULL_IMG'
+    article_info['source'] = Source(new_article.source_url).brand.upper(
+    ) if Source(new_article.source_url).brand else 'NULL_BRAND'
+    article_info['url'] = url
 
-        # Change to Response from RestFramework
-        return JsonResponse(article_info)
+    return Response(article_info)
--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,12 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api.apps.ApiConfig',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    "corsheaders",
     'newspaper',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +78,49 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
+# REST API settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+# Change blacklist to true [?]
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+# CORS_ALLOWED_ORIGINS = [ ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
